6440ece9e300c0ca247f7de628ed0c42d4fa1ff5
saeeeem2 <saemi9464@hanmail.net>
2025-01-03 17:44:20 +0900

    데이터전송연습

--- Dev/src/main/java/com/dev/controller/MemberController.java
+++ Dev/src/main/java/com/dev/controller/MemberController.java
@@ -17,6 +17,9 @@ public class MemberController {
 		return "memberMain";
 	}
 	
+	//GET,POST 차이 : GET은 서버의 리소스에서 데이터를 요청할 때, POST는 서버의 리소스를 새로 생성하거나 업데이트할 때 사용
+	//DB로 따지면 GET은 SELECT, POST는 CREATE 라고 생각하면 됨
+	
 	@RequestMapping(value = "/member/select", method = RequestMethod.POST)
 	public String memberView(@ModelAttribute MemberVO memberVO, Model model) {
 		
